d14fa9442ec56b06bb464c43126ab6c281ff3c1e
Emilly <emilly.wanjiku@student.moringaschool.com>
2023-08-25 15:34:26 +0300

    Create components and pages folder; create landing page file in the pages folder

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import LandingPage from "./Pages/LandingPage";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
        <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <LandingPage/>
     </div>
   );
 }
